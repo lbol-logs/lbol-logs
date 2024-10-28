baec8f4128771e773d9648fc8dbe03a3dd4b54cf
imat-cww <imat.cww@gmail.com>
2024-10-28 13:37:54 +0900

    Updates

---
+++ static/js/9294.76d3f6bb.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklbol_logs_dev=self.webpackChunklbol_logs_dev||[]).push([[9294],{9294:s=>{s.exports=JSON.parse('{"documents":{"C:\\\\Users\\\\vcg020\\\\Documents\\\\GitHub\\\\lbol-logs-dev\\\\public\\\\mods\\\\*":"https://raw.githubusercontent.com/lbol-logs/mods/cad1dc5150411468b80f6e4a84bce48f4f1d953e/*"}}')}}]);
