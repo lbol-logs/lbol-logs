b140408a74565fcd7843e9e915574595c962bbff
imat-cww <imat.cww@gmail.com>
2024-11-19 18:40:59 +0900

    Updates

--- static/js/60263.8ec1540b.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunklbol_logs_dev=self.webpackChunklbol_logs_dev||[]).push([[60263],{60263:e=>{e.exports=JSON.parse('[{"id":"demoKoishi","name":"","character":"Koishi","type":"A","shining":"KoishiG","difficulty":"Normal","requests":[],"result":"TrueEnd","timestamp":"2024-11-13T09:46:59Z"}]')}}]);
